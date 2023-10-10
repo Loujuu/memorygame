d70b311de098f1114d71995669cb6dfe84fa1cee
louise <loujn@vestas.com>
2023-10-10 12:45:23 +0200

    adding store cards

--- scripts.js
+++ scripts.js
@@ -2,11 +2,31 @@
 /*This returns a list of all <div> elements within the document with a class of memory-card*/
 const cards = document.querySelectorAll(".memory-card");
 
+/*Has to know if player clicks first or second card */
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
   //   console.log("I was clicked!");
   //   console.log(this);
-  this.classList.toggle("flip");
   /*toggle means if class is there add it, if not remove*/
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+    //console.log({hasFlippedCard, firstCard});
+  } else {
+    //second click
+    hasFlippedCard = false;
+    secondCard = this;
+    //console.log({ firstCard, secondCard});
+
+    //do cards match?
+    console.log(firstCard.dataset.framework);
+    console.log(secondCard.dataset.framework);
+  }
 }
 
 /*loop throught the list. Add eventlistner to each card to listen for click event, whenever event is fired, will execute a flipcard function */
